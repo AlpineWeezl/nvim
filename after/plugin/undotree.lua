b56c86822786ef3560a9c0fc0637f2ae6a00ae84
Matthias Wieser <64683762+AlpineWeezl@users.noreply.github.com>
2024-01-10 16:17:50 +0100

    feat: configure undotree key (#5)
    Co-authored-by: Matthias Wieser <extern.wieser_matthias@allianz.de>

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,1 +1,8 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+local wk = require('which-key')
+
+wk.register({
+	u = {vim.cmd.UndotreeToggle, 'Undotree'}
+}, { prefix = '<leader>' })
+:w
+:so
+
