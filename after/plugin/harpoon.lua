c9dbbf169e172be0c19bf0e2052f80e04c256028
Matthias Wieser <64683762+AlpineWeezl@users.noreply.github.com>
2024-01-10 16:14:13 +0100

    feat: configure harpoon keys (#4)
    Co-authored-by: Matthias Wieser <extern.wieser_matthias@allianz.de>

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,11 +1,16 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local wk = require('which-key')
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+wk.register({
+h = {
+	name = 'Harpoon',
+	a = {mark.add_file, 'Add File to Harpoon'},
+	q = {ui.toggle_quick_menu, 'Toggle Harpoon Quick Menu'},
+	h = {ui.nav_file(1), 'Harpoon File (1)'},
+	j = {ui.nav_file(2), 'Harpoon File (2)'},
+	k = {ui.nav_file(3), 'Harpoon File (3)'},
+	l = {ui.nav_file(4), 'Harpoon File (4)'},
+}
+},{ prefix = '<leader>' })
 
