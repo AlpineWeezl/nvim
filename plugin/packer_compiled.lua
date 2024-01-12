e66f4374a22392730ed4dc8177e10e198d37b6de
Matthias Wieser <64683762+AlpineWeezl@users.noreply.github.com>
2024-01-12 10:08:16 +0100

    feat: install auto save (#10)
    Co-authored-by: Matthias Wieser <extern.wieser_matthias@allianz.de>

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/matthias/.cache/nvim/packer_hererocks/2.1.1693350652/share/lua/5.1/?.lua;/home/matthias/.cache/nvim/packer_hererocks/2.1.1693350652/share/lua/5.1/?/init.lua;/home/matthias/.cache/nvim/packer_hererocks/2.1.1693350652/lib/luarocks/rocks-5.1/?.lua;/home/matthias/.cache/nvim/packer_hererocks/2.1.1693350652/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/matthias/.cache/nvim/packer_hererocks/2.1.1693350652/lib/lua/5.1/?.so"
+local package_path_str = "/Users/matthias.wieser/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/Users/matthias.wieser/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/Users/matthias.wieser/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/Users/matthias.wieser/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/matthias.wieser/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,111 +76,120 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["autosave.nvim"] = {
+    loaded = true,
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/autosave.nvim",
+    url = "https://github.com/nullishamy/autosave.nvim"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["dracula.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/dracula.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/dracula.nvim",
     url = "https://github.com/Mofiqul/dracula.nvim"
   },
   harpoon = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-ripgrep"] = {
     config = { "\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17nvim-ripgrep\frequire\0" },
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/nvim-ripgrep",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/nvim-ripgrep",
     url = "https://github.com/rinx/nvim-ripgrep"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["rose-pine"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/rose-pine",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   undotree = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/undotree",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["which-key.nvim"] = {
     config = { "\27LJ\2\nt\0\0\3\0\a\0\0156\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0)\1,\1=\1\3\0006\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\15timeoutlen\ftimeout\6o\bvim\0" },
     loaded = true,
-    path = "/home/matthias/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    path = "/Users/matthias.wieser/.local/share/nvim/site/pack/packer/start/which-key.nvim",
     url = "https://github.com/folke/which-key.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: which-key.nvim
+time([[Config for which-key.nvim]], true)
+try_loadstring("\27LJ\2\nt\0\0\3\0\a\0\0156\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0)\1,\1=\1\3\0006\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\15timeoutlen\ftimeout\6o\bvim\0", "config", "which-key.nvim")
+time([[Config for which-key.nvim]], false)
 -- Config for: nvim-ripgrep
 time([[Config for nvim-ripgrep]], true)
 try_loadstring("\27LJ\2\n>\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\17nvim-ripgrep\frequire\0", "config", "nvim-ripgrep")
@@ -189,10 +198,6 @@ time([[Config for nvim-ripgrep]], false)
 time([[Config for rose-pine]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
 time([[Config for rose-pine]], false)
--- Config for: which-key.nvim
-time([[Config for which-key.nvim]], true)
-try_loadstring("\27LJ\2\nt\0\0\3\0\a\0\0156\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0)\1,\1=\1\3\0006\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\15timeoutlen\ftimeout\6o\bvim\0", "config", "which-key.nvim")
-time([[Config for which-key.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
